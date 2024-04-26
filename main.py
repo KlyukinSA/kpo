fd684713ecea38aac616b8e8d0540c126926f10e
KlyukinSA <ctepa.kl@mail.ru>
2024-04-26 11:11:59 +0300

    /update and /predict

--- main.py
+++ main.py
@@ -25,9 +25,62 @@ bot = telebot.TeleBot(environ['KPO_TOKEN'])
 
 field_descs = ["возраст", "пол (1 = М; 0 = Ж)", "тип боли в груди (4 варианта, число)", "артериальное давление в состоянии покоя", "холесторал в сыворотке, мг/дл", "уровень сахара в крови натощак > 120 мг/дл (число 0 или 1)", "результаты электрокардиографии покоя (значения 0,1,2)", "достигнута ли максимальная частота сердечных сокращений", "стенокардия, вызванная физической нагрузкой", "oldpeak = депрессия ST, вызванная физическими упражнениями, по сравнению с отдыхом", "наклон пикового сегмента ST при нагрузке", "количество магистральных сосудов (0-3), окрашенных флюороскопией", "thal: 0 = нормально; 1 = фиксированный дефект; 2 = обратимый дефект"]
 
+filed_names = "age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal".split(',')
+
+def handle_value_error(message):
+    bot.reply_to(message, 'Слушайте введите число а. Дробную часть писать через точку')
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Добро пожаловать! Пожалуйста, введите значения полей в последовательных сообщениях (age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal). В результате мы вам ответим предположением о наличии заболеваний. Начните вводить ' + field_descs[0] + '.')
+
+@bot.message_handler(commands=['update'])
+def update_filed(message):
+    user_id = message.chat.id
+    if 'user_data' not in globals() or user_id not in user_data:
+        bot.reply_to(message, 'Вас еще нет')
+        return
+    
+    parts = message.text.split()
+    if len(parts) < 3:
+        bot.reply_to(message, 'usage: /update age 18')
+        return
+    field = parts[1]
+    value = parts[2]
+
+    step = len(user_data[user_id])
+    i = 0
+    while i < len(filed_names):
+        if filed_names[i] == field:
+            break
+        i += 1
+    if i == len(filed_names):
+        bot.reply_to(message, 'Нет такого поля')
+        return
+    if i > step:
+        bot.reply_to(message, 'У вас еще нет этого поля')
+        return
+    
+    try:
+        value = float(value)
+    except ValueError:
+        handle_value_error(message)
+        return
+    user_data[user_id][i] = value
+
+def predict(user_id):
+    if 'user_data' not in globals() or user_id not in user_data:
+        bot.reply_to(message, 'Вас еще нет')
+        return
+    if len(user_data[user_id]) == len(field_descs):
+        prediction = model.predict(np.array(user_data[user_id]).reshape(1, -1))[0]
+        bot.send_message(user_id, 'Вы' + (' не' if prediction == 0 else '') + ' больны')
+    else:
+        bot.reply_to(message, 'У вас еще не все поля')
+
+@bot.message_handler(commands=['predict'])
+def handle_predict_command(message):
+    predict(message.chat.id)
 
 @bot.message_handler(content_types='text')
 def input_one(message):
@@ -45,21 +98,22 @@ def input_one(message):
 
     step = len(user_data[user_id])
     
-    # if step == 9:
+    if step > len(field_descs) - 1:
+        bot.reply_to(message, 'Все вы все заполнили что вам еще надо?? Ну можно обновлять поля (/update age 18). Еще раз узнать результат будет /predict')
+        return
+
     try:
         value = float(raw)
     except ValueError:
-        bot.reply_to(message, 'Слушайте введите число а. Дробную часть писать через точку.')
+        handle_value_error(message)
         return
     
     if value != None:
         user_data[user_id].append(value)
 
     if step == len(field_descs) - 1:
-        prediction = model.predict(np.array(user_data[user_id]).reshape(1, -1))[0]
-        bot.send_message(user_id, 'Вы' + (' не' if prediction == 0 else '') + ' больны')
-        del user_data[user_id]
-
-    bot.send_message(user_id, 'Введите ' + field_descs[(step + 1) % len(field_descs)])
+        predict(user_id)
+    else:
+        bot.send_message(user_id, 'Введите ' + field_descs[(step + 1) % len(field_descs)])
     
 bot.polling()
