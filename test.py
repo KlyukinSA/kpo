9f868a789f079f1fa0e220b8e82dcb55c09f986f
KlyukinSA <ctepa.kl@mail.ru>
2024-05-07 12:32:02 +0300

    add tests for /predict and /update

--- test.py
+++ test.py
@@ -4,47 +4,58 @@ from time import sleep
 
 from main import bot
 
-# Emulate Telegram API for bot
 telemul = Telemulator()
 telemul.set_tested_bot(bot, username='my_bot', name='My Bot')
 
-# At start, there are no registered users in emulated API.
 assert not telemul.api.users
 
-# Make API user, that represent our bot.
-# It's a first registered user.
 mybot = telemul.api.get_me()
 assert mybot.is_bot
 assert mybot.username == 'my_bot'
+
 assert len(telemul.api.users) == 1
 
-# New user open private chat with bot and send `/start` command.
-# Bot must answer as defined and his answer must be in chat history.
-user = telemul.api.create_user('User')
-chat = user.private()
+def test_main_user_scenario_predict(call_n):
+    user = telemul.api.create_user('User' + str(call_n))
+    chat = user.private()
+    iteration = 0
+    def send_check_reply(message, reply):
+        nonlocal iteration
+        iteration += 2
+        send_command(chat, message, user)
+        sleep(0.05)
+        assert reply in str(chat.history.messages[iteration])
+    # 44,1,2,130,234,0,1,179,1,0.4,2,0,2
+    send_check_reply('/start', "Начните вводить возраст")
+    send_check_reply('44', "Введите пол")
+    send_check_reply('1', "Введите тип боли")
+    send_check_reply('2', "артериальное давление")
+    send_check_reply('130', "холесторал в сыворотке")
+    send_check_reply('234', "уровень сахара")
+    send_check_reply('0', "результаты электрокардиографии")
+    send_check_reply('1', "максимальная частота сердечных")
+    send_check_reply('179', "стенокардия")
+    send_check_reply('1', "oldpeak")
+    send_check_reply('0.4', "наклон пикового сегмента")
+    send_check_reply('2', "магистральных сосудов")
+    send_check_reply('0', "thal")
+    send_check_reply('2', "Вы больны")
+    send_check_reply('/predict', "Вы больны")
 
-iteration = 0
-def send_check_reply(message, reply):
-    global iteration
-    iteration += 2
-    send_command(chat, message, user)
+def test_update_all_scenarios(call_n):
+    user = telemul.api.create_user('User' + str(call_n))
+    chat = user.private()
+    send_command(chat, '42', user)
+    send_command(chat, "/update age 52", user)
     sleep(0.05)
-    assert reply in str(chat.history.messages[iteration])
+    assert len(chat.history.messages) == 3
+    send_command(chat, "/update thal 52", user)
+    sleep(0.05)
+    assert 'У вас еще нет этого поля' in str(chat.history.messages[5])
+    send_command(chat, "/update asd 52", user)
+    sleep(0.05)
+    assert 'Нет такого поля' in str(chat.history.messages[7])
 
-send_check_reply('/start', "Начните вводить возраст")
-send_check_reply('44', "Введите пол")
-send_check_reply('1', "Введите тип боли")
-send_check_reply('2', "артериальное давление")
-send_check_reply('130', "холесторал в сыворотке")
-send_check_reply('234', "уровень сахара")
-send_check_reply('0', "результаты электрокардиографии")
-send_check_reply('1', "максимальная частота сердечных")
-send_check_reply('179', "стенокардия")
-send_check_reply('1', "oldpeak")
-send_check_reply('0.4', "наклон пикового сегмента")
-send_check_reply('2', "магистральных сосудов")
-send_check_reply('0', "thal")
-send_check_reply('2', "Вы больны")
-
-# 44,1,2,130,234,0,1,179,1,0.4,2,0,2
-# 1
+test_functions = [test_main_user_scenario_predict, test_update_all_scenarios]
+for i in range(len(test_functions)):
+    test_functions[i](i)
